2bcd1043f4f4eb0b6d9157fdeac5cb8a75348a54
Rubydog <hirabizz@gmail.com>
2024-04-17 12:27:33 +0900

    視聴者さんの質問に回答

---
+++ lib/qa/others/todo_example2/check_box_new.dart
@@ -0,0 +1,48 @@
+import 'package:banana/qa/others/todo_example2/custom_checkbok.dart';
+import 'package:banana/qa/others/todo_example2/todos_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/* 自分のアプリ名によって変わるコード */
+// import 'package:simple_memo/application/state/checked_ids.dart';
+// import 'package:simple_memo/presentation/theme/colors.dart';
+
+/* ルビーDogはこっち */
+import 'package:banana/qa/others/todo_example2/checked_ids.dart';
+import 'package:banana/qa/others/todo_example2/colors.dart';
+
+class CheckBox extends ConsumerWidget {
+  const CheckBox({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    /// Todoモデルたち
+    final models = ref.watch(todoModelsNotifierProvider);
+
+    /// チェックされたIDたち....例えば [ A, C ] なら AとCがチェックされている
+    final checkedIds = ref.watch(checkedIdsNotifierProvider);
+
+    void onChangedCheckbox(String id) {
+      // ノティファイヤーを使って状態を変える
+      final notifier = ref.read(checkedIdsNotifierProvider.notifier);
+      notifier.onTapCheckbox(id);
+    }
+
+    final col = Column(
+      children: [
+        // Todoモデルの数だけ繰り返す
+        for (final model in models)
+          CustomCheckbox(
+            activeColor: BrandColor.darkGrey,
+            onChanged: (check) => onChangedCheckbox(model.id),
+            value: checkedIds.contains(model.id),
+            title: Text(model.memo),
+          ),
+      ],
+    );
+
+    return Scaffold(
+      body: col,
+    );
+  }
+}
--- lib/qa/others/todo_example2/todos_provider.g.dart
+++ lib/qa/others/todo_example2/todos_provider.g.dart
@@ -1,27 +1,27 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'checked_ids.dart';
+part of 'todos_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$checkedIdsNotifierHash() =>
-    r'4e53aa8b6926cfc5ffba9487d67d61d03081b995';
+String _$todoModelsNotifierHash() =>
+    r'4ce9aa549404cedde2563dfac911589eaeaac87b';
 
-/// See also [CheckedIdsNotifier].
-@ProviderFor(CheckedIdsNotifier)
-final checkedIdsNotifierProvider =
-    AutoDisposeNotifierProvider<CheckedIdsNotifier, List<String>>.internal(
-  CheckedIdsNotifier.new,
-  name: r'checkedIdsNotifierProvider',
+/// See also [TodoModelsNotifier].
+@ProviderFor(TodoModelsNotifier)
+final todoModelsNotifierProvider =
+    AutoDisposeNotifierProvider<TodoModelsNotifier, List<TodoModel>>.internal(
+  TodoModelsNotifier.new,
+  name: r'todoModelsNotifierProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$checkedIdsNotifierHash,
+      : _$todoModelsNotifierHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$CheckedIdsNotifier = AutoDisposeNotifier<List<String>>;
+typedef _$TodoModelsNotifier = AutoDisposeNotifier<List<TodoModel>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
