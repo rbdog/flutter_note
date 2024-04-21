58a9ff1322685905a13cbe8c9310db9b965bf750
rbdog <rbdog@rubydog.jp>
2024-04-21 13:03:54 +0900

    視聴者さんの質問に回答

--- lib/qa/others/todo_example2/check_box.dart
+++ lib/qa/others/todo_example2/check_box.dart
@@ -1,56 +1,45 @@
+import 'package:banana/qa/others/todo_example2/add_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /* 自分のアプリ名によって変わるコード */
-// import 'package:simple_memo/application/state/checked_ids.dart';
 // import 'package:simple_memo/presentation/theme/colors.dart';
+// import 'package:simple_memo/.../.../todos_provider.dart';
 
 /* ルビーDogはこっち */
-import 'package:banana/qa/others/todo_example2/checked_ids.dart';
 import 'package:banana/qa/others/todo_example2/colors.dart';
-
-/* これはもういらない。 代わりに checked_ids.dart に書いたものを使う*/
-// final checkedIdsProvider = ...
+import 'package:banana/qa/others/todo_example2/todos_provider.dart';
 
 class CheckBox extends ConsumerWidget {
   const CheckBox({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    /// チェックされたIDたち....例えば [ A, C ] なら AとCがチェックされている
-    final checkedIds = ref.watch(checkedIdsNotifierProvider);
+    /// Todoモデルたち
+    final models = ref.watch(todoModelsNotifierProvider);
 
     void onChangedCheckbox(String id) {
       // ノティファイヤーを使って状態を変える
-      final notifier = ref.read(checkedIdsNotifierProvider.notifier);
-      notifier.onTapCheckbox(id);
+      final notifier = ref.read(todoModelsNotifierProvider.notifier);
+      notifier.toggleCheck(id);
     }
 
     final col = Column(
       children: [
-        CheckboxListTile(
-          activeColor: BrandColor.darkGrey,
-          onChanged: (check) => onChangedCheckbox('A'),
-          value: checkedIds.contains('A'),
-          title: Text('バナナを買う'),
-        ),
-        CheckboxListTile(
-          activeColor: BrandColor.darkGrey,
-          onChanged: (check) => onChangedCheckbox('B'),
-          value: checkedIds.contains('B'),
-          title: Text('バナナを食べる'),
-        ),
-        CheckboxListTile(
-          activeColor: BrandColor.darkGrey,
-          onChanged: (check) => onChangedCheckbox('C'),
-          value: checkedIds.contains('C'),
-          title: Text('バナナを捨てる'),
-        ),
+        // Todoモデルの数だけ繰り返す
+        for (final model in models)
+          CheckboxListTile(
+            activeColor: BrandColor.darkGrey,
+            onChanged: (check) => onChangedCheckbox(model.id),
+            value: model.isChecked,
+            title: Text(model.memo),
+          ),
       ],
     );
 
     return Scaffold(
       body: col,
+      floatingActionButton: const AddButton(),
     );
   }
 }
--- lib/qa/others/todo_example2/todos_provider.dart
+++ lib/qa/others/todo_example2/todos_provider.dart
@@ -1,4 +1,4 @@
-import 'package:banana/qa/others/todo_example/todo_model.dart';
+import 'package:banana/qa/others/todo_example2/todo_model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'todos_provider.g.dart';
 
@@ -7,29 +7,61 @@ class TodoModelsNotifier extends _$TodoModelsNotifier {
   @override
   List<TodoModel> build() {
     return const [
-      TodoModel(id: 'A', memo: 'あいうえお'),
-      TodoModel(id: 'B', memo: 'かきくけこ'),
-      TodoModel(id: 'C', memo: 'さしすせそ'),
+      TodoModel(id: 'A', memo: 'あいうえお', isChecked: false),
+      TodoModel(id: 'B', memo: 'かきくけこ', isChecked: true),
+      TodoModel(id: 'C', memo: 'さしすせそ', isChecked: false),
     ];
   }
 
-  // 新しい ToDo を追加する
+  /// 新しい ToDo を追加する
   void addNewTodo() {
     final newModels = List.of(state);
-    const newModel = TodoModel(id: 'D', memo: 'たちつてと');
+    const newModel = TodoModel(id: 'D', memo: 'たちつてと', isChecked: false);
     newModels.add(newModel);
     state = newModels;
   }
 
-  // メモを編集する
-  void updateMemo(String id, String memo) {
-    final newModel = TodoModel(id: id, memo: memo);
+  /// チェックON/OFFを切り替える
+  void toggleCheck(String id) {
+    // 切り替え前のデータ
+    final oldModel = state.firstWhere((it) => it.id == id);
+    // 切り替え後のデータ
+    final newModel = TodoModel(
+      id: oldModel.id,
+      memo: oldModel.memo,
+      isChecked: !(oldModel.isChecked), // ! で true/false を反転できる
+    );
+
+    // 新しいデータたち(全てのTodo)を準備する
     final newModels = state.map(
       (it) {
         if (it.id == id) return newModel;
         return it;
       },
     ).toList();
+    // データを上書き
+    state = newModels;
+  }
+
+  /// メモを編集する
+  void updateMemo(String id, String memo) {
+    // 切り替え前のデータ
+    final oldModel = state.firstWhere((it) => it.id == id);
+    // 切り替え後のデータ
+    final newModel = TodoModel(
+      id: oldModel.id,
+      memo: memo,
+      isChecked: oldModel.isChecked,
+    );
+
+    // 新しいデータたち(全てのTodo)を準備する
+    final newModels = state.map(
+      (it) {
+        if (it.id == id) return newModel;
+        return it;
+      },
+    ).toList();
+    // データを上書き
     state = newModels;
   }
 }
